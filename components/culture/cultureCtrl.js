0e3a20624f43d0d13b9ad77de0e3f2f15206c607
amaryana <amarpatel1290@gmail.com>
2016-04-20 17:56:27 -0600

    work

--- components/culture/cultureCtrl.js
+++ components/culture/cultureCtrl.js
@@ -1,4 +1,15 @@
-angular.module('cosmicdigest').controller('cultureCtrl', function($scope){
+angular.module('cosmicdigest').controller('cultureCtrl', function($scope, FirebaseService, $state){
+ // console.log($state.current.name);
+
+ $scope.getPosts = function (section) {
+   FirebaseService.getPosts(section).then(function(res){
+     $scope.culture = res.data;
+     console.log($scope.posts);
+
+   })
+ }
+ $scope.getPosts('culture');
+
 
 
 });
