3abab998ed173ce23aa3a985cb1b377b135befd5
amaryana <amarpatel1290@gmail.com>
2016-04-20 18:19:16 -0600

    please

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+
 var port = 80;
-
-app.use(express.static(__dirname+'/../public'));
-
+// app.listen(port, function(){
+//  console.log("Listeing on port " + port);
+// });
 app.listen(port, function(){
- console.log("Listeing on port ", port);
-});
+  console.log("Listening to aliens on port " + port);
+})
